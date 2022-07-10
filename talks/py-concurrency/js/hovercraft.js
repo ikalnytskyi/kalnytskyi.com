aabad2448efc2e4dd44dd727a3c921298475b69f
Ihor Kalnytskyi <ihor@kalnytskyi.com>
2022-07-10 15:19:08 +0300

    Regenerate py-concurrency slide deck
    It turns out that I still have sources, so I can regenerate the
    slide deck from scratch fixing issues along the way:
    
     * Code snippets are properly formatted now
     * Cyrillic letters aren't escaped
     * HTML is pretty formatted

--- talks/py-concurrency/js/hovercraft.js
+++ talks/py-concurrency/js/hovercraft.js
@@ -1,37 +1,37 @@
-// Initialize impress.js
-impress().init();
+// Main hovercraft js file
+( function( document ) {
+    "use strict";
 
-// Set up the help-box
-var helpdiv = window.document.getElementById('hovercraft-help');
+    document.addEventListener( "impress:init", function( event ) {
 
-if (window.top!=window.self) {
-    // This is inside an iframe, so don't show the help.
-    helpdiv.className = "disabled";
-
-} else {
-    // Install a funtion to toggle help on and off.
-    var help = function() {
-        if(helpdiv.className == 'hide')
-            helpdiv.className = 'show';
-        else
-            helpdiv.className = 'hide';
-    };
-    impressConsole().registerKeyEvent([72], help, window);
-
-    // The help is by default shown. Hide it after five seconds.
-    setTimeout(function () {
-        var helpdiv = window.document.getElementById('hovercraft-help');
-        if(helpdiv.className != 'show')
-            helpdiv.className = 'hide';
-    }, 5000);
-}
+        var api = event.detail.api;
+        var util = api.lib.util;
 
 
-if (impressConsole) {
-    impressConsole().init(cssFile='css/impressConsole.css');
+        // Set up the help-box
+        // Overwrite the default navigation help
+        util.triggerEvent( document, "impress:help:add", { command: "Left, Down, Page Down, Space",
+                                                           text: "Next step",
+                                                           row: 1 } );
+        util.triggerEvent( document, "impress:help:add", { command: "Right, Up, Page Up",
+                                                           text: "Previous step",
+                                                           row: 2 } );
+    });
 
-    var impressattrs = document.getElementById('impress').attributes
+    // Initialize impress.js
+    impress().init();
+
+    var impressattrs = document.getElementById('impress').attributes;
     if (impressattrs.hasOwnProperty('auto-console') && impressattrs['auto-console'].value.toLowerCase() === 'true') {
-        consoleWindow = console().open();
+        impressConsole().open();
     }
+
+}) (document);
+
+
+// Function updating the slide number counter
+function update_slide_number(evt)
+{
+    var step = evt.target.attributes['step'].value;
+    document.getElementById('slide-number').innerText = parseInt(step) + 1;
 }
